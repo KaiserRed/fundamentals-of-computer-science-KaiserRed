aca87c501b7297d57eaee07e2892c47e33cb71b6
Alexey <113765498+KaiserRed@users.noreply.github.com>
2022-11-14 18:10:55 +0300

    Update lab9.c

--- labs/lab-10/lab9.c
+++ labs/lab-10/lab9.c
@@ -1,54 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct{
+    int i, j, l;
+}ring;
+
 int max(int a, int b);
 int min(int a, int b);
 int sign(int a);
 
 int main(void){
-    int i=1, j=-30, l=1, ik=i, jk=j, lk=l;
-    for (int k=0;k<50;k++){
-        ik=i;
-        jk=j;
-        lk=l;
-        i=(max(min(ik+jk-lk-k,ik-jk+lk-k), min(k+ik-jk-lk, k-ik-jk+lk)));
-        j=(jk+lk*sign(jk)%20);
-        l=abs(ik-jk+lk-k)*sign(ik)*sign(jk);
-        if ((i-10)*(i-10)+(j-10)*(j-10)<=25){  
-            printf("Попадание в заданную область %d %d %d %d\n", k,i,j,l);
-            return 0;
+    ring s = {1,-30,1};
+    for (int k = 0; k < 50; k++){
+        ring b = {s.i,s.j,s.l};
+        s.i = max(
+              min(b.i + b.j - b.l -k, b.i - b.j + b.l - k),
+              min(k + b.i - b.j - b.l, k - b.i - b.j + b.l)
+        );
+        s.j = b.j + (b.l * sign(b.j) % 20) + k * sign(b.i) % 10;
+        s.l=abs(b.i - b.j + b.l - k) * sign(b.i) * sign(b.j);
+        printf("Step X Y Movement %d %d %d %d\n",k, s.i, s.j ,s.l);
+        if ((((s.i - 10) * (s.i - 10)) + ((s.j - 10) * (s.j - 10)) >= 25)
+         & (((s.i - 10) * (s.i - 10)) + ((s.j - 10) * (s.j - 10)) <=100)){  
+            printf("Point has reached destination %d %d %d %d\n", k, s.i ,s.j ,s.l);
         }
      
     }
-    printf("Не попал%d %d %d\n",i,j,l);
+ 
+    printf("Point has not reached destination%d %d %d\n",s.i,s.j,s.l);
     return 0;
     
 }
 int max(int a, int b) {
-    if (a>b) {
-        return a;
-    }
-    else {
-        return b;
-    }
+    return a < b ? b : a;
 }
 
 int min(int a, int b) {
-    if (a>b) {
-        return b;
-    }
-    else {
-        return a;
-    }
+    return a > b ? b : a;
 }
 int sign(int a) {
-    if (a>0) {
-        return 1;
-    }
-    if (a==0){
-        return 0;
-    }
-    else {
-        return -1;
-    }
+    return a > 0 ? 1 : a < 0 ? -1 : 0;
 }
+
