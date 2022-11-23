f8c2508a647730308c024cf1ca0a9d738c067527
Alexey <113765498+KaiserRed@users.noreply.github.com>
2022-11-23 10:30:26 +0300

    Update lab12.c

--- labs/lab-12/lab12.c
+++ labs/lab-12/lab12.c
@@ -26,6 +26,7 @@ int Digit_count () {
         switch (state) {
             case ZERO:
                 if ((isdigit(c) != 0)){
+                    t = 0;
                     q.i = c;
                     if ((b == 1)){
                         s.l = c;
@@ -35,10 +36,18 @@ int Digit_count () {
                         y.j = c;
                         m = 2;
                     }
+                    if ((b == 2) & (isdigit(s.i) != 0) & (s.i < s.j) & (s.j < s.l)){
+                        printf("%c%c%c\n", s.i, s.j, s.l);
+                        b = 0;
+                        s.i = 0;
+                        s.j = 0;
+                        s.l = 0;
+                    }  
                     state = ONE;
                 }
                 break;
            case ONE:
+               b = 0;
                s.i = c;
                q.j = c;
                state = TWO;
@@ -46,32 +55,38 @@ int Digit_count () {
                    m = 3;
                    y.l = c;
                }
+               if ((m == 3) & (isdigit(y.i) != 0) & (y.i < y.j) & (y.j < y.l)){
+                   printf("%c%c%c\n", y.i, y.j, y.l);
+                   m = 0;
+                   y.i = 0;
+                   y.j = 0;
+                   y.l = 0;
+               }
+               
                break;
           case TWO:
                 y.i = c;
                 m = 1;
-                 q.l = c;
-                 t = 1;
-                 state = ZERO;
-                  if ((int) s.i < (int) c){
-                        s.j = c;
-                        b = 1;
-                   } 
+                q.l = c;
+                t = 1;
+                if ((isdigit(q.i) != 0) & (q.i < q.j) & (q.j < q.l)){
+                    printf("%c%c%c\n", q.i, q.j, q.l);
+                    t = 0;
+                    q.i = 0;
+                    q.j = 0;
+                    q.l = 0;
+                }
+                state = ZERO;
+                if ((int) s.i < (int) c){
+                    s.j = c;
+                    b = 1;
+                } 
                 break;
-            }
-
-        if ((t == 1) & (q.i < q.j) & (q.j < q.l)){
-            printf("= %c %c %c\n", q.i, q.j, q.l);
-            t = 0;
-        }
-        if ((b == 2) & (s.i < s.j) & (s.j < s.l)){
-            printf("= %c %c %c\n", s.i, s.j, s.l);
-            b = 0;
-        }
-        if ((m == 3) & (isdigit(y.i) != 0) & (y.i < y.j) & (y.j < y.l)){
-            printf("= %c %c %c\n", y.i, y.j, y.l);
-            m = 0;
+            
+        
+        
         }
     }
     return 0;
 }
+
