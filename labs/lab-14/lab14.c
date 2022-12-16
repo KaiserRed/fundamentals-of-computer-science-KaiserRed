b470cadbf5a07d4717548b15a1df3fe9365ef48e
Alexey <113765498+KaiserRed@users.noreply.github.com>
2022-12-17 01:51:04 +0300

    Update lab14.c

--- labs/lab-14/lab14.c
+++ labs/lab-14/lab14.c
@@ -1,27 +1,38 @@
 #include <stdio.h>
 
-enum { maxn = 100 };
+enum { maxn = 10 };
+void matrix_input(int n, int (*m)[n]);
+void matrix_output(int n, int (*m)[n]);
 
-int main(){
+int main(void){
     int m[maxn][maxn];
     int n;
     scanf("%d", &n);
+    matrix_input(n, (int (*)[n]) m);
+    matrix_output(n, (int (*)[n]) m);
+    return 0;
+} 
+
+void matrix_input(int n, int (*m)[n]){
     for (int i = 0; i != n; ++i)
         for (int j = 0; j != n; ++j)
             scanf("%d", &m[i][j]);
+}
+
+void matrix_output(int n, int (*m)[n]){
     int l = n;
     int i = l - 1, j = l - 1;
     while (i != 0 & j != 0){
         j = i - 1;
         i = l;
         if (i != n)
-            printf("%d\n", m[i][j]);
+            printf("%d ", m[i][j]);
         while (j != l & j < n){
             i-=1;
             j+=1;
             if (j == n)
                 break;
-            printf("%d\n", m[i][j]);
+            printf("%d ", m[i][j]);
         }
         if (i == 0){
             l-=1;
@@ -30,5 +41,4 @@ int main(){
     
     }
     putchar('\n');
-    return 0;
-}  
+}
