feda0bc75358c744bc72353206774e657c70ebc0
Alexey <113765498+KaiserRed@users.noreply.github.com>
2023-01-10 12:35:35 +0300

    Update CP4.c

--- CP/СP4/CP4.c
+++ CP/СP4/CP4.c
@@ -8,14 +8,20 @@ typedef long double ldbl;
 ldbl mach_eps();
 
 dbl func (dbl x);
+dbl func_2 (dbl x);
 dbl Df(dbl x);
+dbl DF_2 (dbl x);
+dbl DDF_2 (dbl x);
 dbl f (dbl x);
 dbl Iteration(dbl f(dbl), dbl a, dbl b);
+dbl Newton(dbl func(dbl), dbl a, dbl b);
 
 int main(){
-    dbl a = 1, b = 1.5;
+    dbl a = 1, b = 1.5, a2 = 1, b2 = 3;
     assert(fabs(Df(a)) < 1);
+    assert(fabs(func_2(a2)*DDF_2(a2)) < pow(DF_2(a2),2));
     printf("Iteration method result for func: %f\n", Iteration(f, a, b));
+    printf("Newton method result for func_2: %f\n", Newton(func_2, a2, b2));
     return 0;
 }
 
@@ -30,20 +36,41 @@ ldbl mach_eps(){
 dbl func (dbl x){
     return (1 - x + sin(x) - log(1+x));
 }
-
+dbl func_2 (dbl x){
+    return (3*x - 14 + exp(x) - exp(-x));
+}
 dbl f (dbl x){
     return (1 + sin(x) - log(1+x));
 }
 
 dbl Df (dbl x){
-    return (-1 + cos(x) - (1/(1+x)));
+    return (cos(x) - (1/(1+x)));
+}
+
+dbl DF_2 (dbl x){
+    return (3 + exp(x) - exp(-x));
+}
+
+dbl DDF_2 (dbl x){
+    return (exp(x) - exp(-x));
 }
 
 dbl Iteration(dbl f(dbl), dbl a, dbl b){
     ldbl eps = mach_eps ();
-    dbl x = a + (b - a)/2;
+    dbl x = a + (b - a) / 2;
     while (fabs(f(x) - x) >= eps){
         x = f(x);
     }
     return x;
+}
+
+dbl Newton(dbl func(), dbl a, dbl b){
+    ldbl eps = mach_eps ();
+    dbl x = a + (b - a) / 2;
+    dbl x2 = x - (func(x) / DF_2(x));
+    while (fabs(x - x2) >= eps){
+        x = x2;
+        x2 = x - (func_2(x) / DF_2(x));
+    }
+    return x2;
 }
